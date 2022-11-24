2adb2bc9974384387867fb18089c1aac87c48d86
Shivanshp4s <79980567+Shivanshp4s@users.noreply.github.com>
2022-11-24 00:23:59 -1200

    Update and rename 102.py to 103.py

--- 103.py
+++ 103.py
@@ -8,7 +8,7 @@ from_dir = "C:/Users/DELL/Downloads"
 to_dir = "C:/Users/DELL/Downloads/document"
 
 list_of_files = os.listdir(from_dir)
-#print(list_of_files)
+
 
 class FileMovementHandler(FileSystemEventHandler):
     def on_created(self,event):
@@ -17,16 +17,16 @@ class FileMovementHandler(FileSystemEventHandler):
     def on_deleted(self,event):
         print({event.src_path},"Oops!this file has been deleted")
 
-#initalising event handler class
+
 event_handler = FileMovementHandler()
 
-#initialising observer
+
 observer = Observer()
 
-#Schedule the observer
+
 observer.schedule(event_handler,from_dir,recursive=True)
 
-#start the observer
+
 observer.start()
             
 while True :
